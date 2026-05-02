865b40d7194128d1c6698daf22131b1caef2ce8f
bfmhno3 <858446559@qq.com>
2026-05-02 20:55:57 +0800

    feat(modules): implement wifi_module module
    Add WiFi camera expansion module that receives 8-byte control frames
    over USART1 (19200 baud) and feeds roll/pitch/yaw/thrust to the
    commander subsystem via CTRL_SRC_WIFI.
    
    Protocol: [0x66][roll][pitch][thrust][yaw][cmd][chksum][0x99]

---
+++ Core/Inc/modules/wifi_module.h
@@ -0,0 +1,10 @@
+#ifndef MODULES_WIFI_MODULE_H
+#define MODULES_WIFI_MODULE_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+void wifi_module_init(void);
+void wifi_module_deinit(void);
+
+#endif /* MODULES_WIFI_MODULE_H */
